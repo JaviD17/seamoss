81033d3bf9b7fe733aadee22511d4ea69ab5a2d3
Javier Dominguez II <Javi7057@gmail.com>
2023-01-05 21:17:34 -0600

    Navbar css done along with framer motion animations, added hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
 import Card from "./components/Card";
 import Navbar from "./components/Navbar";
 import { Source_Code_Pro } from "@next/font/google";
+import Image from "next/image";
 
 const inter = Source_Code_Pro({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={`${inter.className} container mx-auto`}>
+    <>
       <Navbar />
-      <h1 className="text-4xl text-center">Welcome to SeaMoss!</h1>
-      <section className="mx-4"></section>
-    </main>
+      <main className={`${inter.className}`}>
+        <Image
+          src="/seamoss.webp"
+          width={1440}
+          height={500}
+          alt="Picture of the author"
+        />
+        <section className="mx-4"></section>
+      </main>
+    </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,30 +8,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#2a9392",
-
-          secondary: "#43ef68",
-
-          accent: "#dd6c85",
-
-          neutral: "#1F262D",
-
-          "base-100": "#E4E2E9",
-
-          info: "#93BBE1",
-
-          success: "#15BC84",
-
-          warning: "#B39814",
-
-          error: "#EB2019",
-        },
-      },
-    ],
-  },
-  plugins: [require("daisyui")],
+  plugins: [],
 };
